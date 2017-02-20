aa0f8aab76c464b6624c7d367fed298e1bda1e8b
Sam Jones <sam.jones494@googlemail.com>
2017-02-20 17:25:44 +0000

    step 11

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -23,13 +23,19 @@ class RestaurantsController < ApplicationController
   def update
     @restaurant = Restaurant.find(params[:id])
     @restaurant.update(restaurant_params)
+    redirect_to '/restaurants'
+  end
 
+  def destroy
+    @restaurant = Restaurant.find(params[:id])
+    @restaurant.destroy
+    flash[:notice] = "Restaurant deleted successfully"
     redirect_to '/restaurants'
   end
 
   private
 
   def restaurant_params
-    params.require(:restaurant).permit(:name, :description)
+    params.require(:restaurant).permit(:name)
   end
 end
