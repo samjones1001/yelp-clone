0548d3503f81bb8699f58118611b1fc2d7751d51
Sam Jones <sam.jones494@googlemail.com>
2017-02-20 18:00:24 +0000

    step 12 WIP

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  resources :restaurants
+  resources :restaurants do
+    resources :reviews
+  end
   get 'restaurants' => 'restaurants#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
