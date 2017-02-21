f0914c84b50586257a6d929581d4b245aa5761af
Sam Jones <sam.jones494@googlemail.com>
2017-02-21 15:06:52 +0000

    step 16 WIP

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :users
+  root "restaurants#index"
   resources :restaurants do
     resources :reviews
   end
---
+++ spec/features/users_feature_spec.rb
@@ -0,0 +1,32 @@
+require 'rails-helper'
+
+feature 'User can sign in and out' do
+  context 'user not signed in and on the hompage' do
+    it "should see a 'sign in'link and a 'sign up' link"
+      visit '/'
+      expect(page).to have_link('Sign in')
+      expect(page).to have_link('Sign up')
+    end
+
+    it "should not see a 'sign out' link" do
+      visit '/'
+      expect(page).not_to have_link('Sign out')
+    end
+  end
+
+  context "user signed in on the home page" do
+    before do
+      visit '/'
+      click_link('Sign up')
+      fill_in('Email', with: 'test@example.com')
+      fill_in('Password', with: 'testtest')
+      fill_in('Password confirmation', with: 'testtest')
+      click_button('Sign up')
+    end
+
+    it "should see 'sign out' link" do
+      visit '/'
+      expect(page).to have_link('Sign out')
+    end
+  end
+end
