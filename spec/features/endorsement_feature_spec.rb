8c25d0f930e8d0b0ac311f297d345a14213d4424
Sam Jones <sam.jones494@googlemail.com>
2017-02-24 20:55:07 +0000

    bonus challenge 5

--- spec/features/endorsement_feature_spec.rb
+++ spec/features/endorsement_feature_spec.rb
@@ -21,9 +21,9 @@ feature 'endorsing reviews' do
     expect(page).to have_content('1 endorsement')
   end
 
-  scenario 'a user can endorse a review, which updates the review endorsement count' do
+  scenario 'a user can endorse a review, which updates the review endorsement count', js: true do
     visit '/restaurants'
-    click_link 'Endorse Review' #are we endorsing restaurants or the review of the restaurants?
+    click_link 'Endorse' #are we endorsing restaurants or the review of the restaurants?
     expect(page).to have_content('1 endorsement')
   end
 
